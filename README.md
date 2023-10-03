b7ff4b1fe1a8bf1e476925aed6eeb02527e6aad2
Emanuel Tesař <e.tesarr@gmail.com>
2023-10-03 14:26:28 +0200

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# airseekerV2
+# airseeker-v2
+
+> A service powering data feeds using the [signed API](https://github.com/api3dao/signed-api).
+
+The Airseeker is a rework of the [original Airseeker](https://github.com/api3dao/airseeker). The Airseeker v2 is
+simplified and only works with signed APIs.
+
+## Getting started
+
+To install the dependencies:
+
+```sh
+pnpm install
+```
---
+++ jest.setup.js
@@ -0,0 +1,20 @@
+// Sometimes Jest can have issues when trying to spy on an internal module. Import
+// and use this function as a workaround at the top of your test.
+//
+// Credit: https://github.com/facebook/jest/issues/6914#issuecomment-654710111
+const { defineProperty } = Object;
+
+Object.defineProperty = function (object, name, meta) {
+  if (meta.get && !meta.configurable) {
+    // it might be an ES6 exports object
+    return defineProperty(object, name, {
+      ...meta,
+      configurable: true, // prevent freezing
+    });
+  }
+
+  return defineProperty(object, name, meta);
+};
+
+// Disable logger if it is not explicitly set to true.
+process.env.LOGGER_ENABLED = process.env.LOGGER_ENABLED || 'false';
