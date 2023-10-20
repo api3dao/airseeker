94ba8abcd72f941236cbda752bb30c946b0c12e9
Emanuel Tesař <e.tesarr@gmail.com>
2023-10-20 12:57:32 +0200

    Add logger from commons

--- src/logger.ts
+++ src/logger.ts
@@ -1,27 +1,11 @@
-const debug = (...args: any[]) =>
-  // eslint-disable-next-line no-console
-  console.debug(...args);
-const error = (...args: any[]) =>
-  // eslint-disable-next-line no-console
-  console.error(...args);
-const info = (...args: any[]) => console.info(...args);
-const log = (...args: any[]) =>
-  // eslint-disable-next-line no-console
-  console.log(...args);
-const warn = (...args: any[]) =>
-  // eslint-disable-next-line no-console
-  console.warn(...args);
+import { createLogger } from '@api3/commons';
+import { loadEnv } from './env/env';
 
-export const logErrors = (promiseResults: PromiseSettledResult<any>[], additionalText = '') => {
-  for (const rejectedPromise of promiseResults.filter((result) => result.status === 'rejected')) {
-    error(additionalText, rejectedPromise);
-  }
-};
+const env = loadEnv();
 
-export const logger = {
-  debug,
-  error,
-  info,
-  log,
-  warn,
-};
+export const logger = createLogger({
+  colorize: env.LOG_COLORIZE,
+  enabled: env.LOGGER_ENABLED,
+  minLevel: env.LOG_LEVEL,
+  format: env.LOG_FORMAT,
+});
