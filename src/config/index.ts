8f10e2219f5e04d3b6120e3d1b53fa9a3a58d52c
aquarat <blue@aquarat.co.uk>
2023-10-27 11:02:52 +0000

    Add README vs example config test

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,20 +1,19 @@
 import fs, { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 
-import { go } from '@api3/promise-utils';
+import { goSync } from '@api3/promise-utils';
 import dotenv from 'dotenv';
 
 import { configSchema } from './schema';
 import { interpolateSecrets, parseSecrets } from './utils';
 
-export const loadConfig = async () => {
-  const configPath = join(__dirname, '../../config');
+export const loadConfig = (configPath = join(__dirname, '../../config'), airseekerConfigPath = 'airseeker.json') => {
   const rawSecrets = dotenv.parse(readFileSync(join(configPath, 'secrets.env'), 'utf8'));
 
-  const goLoadConfig = await go(async () => {
-    const rawConfig = JSON.parse(fs.readFileSync(join(configPath, 'airseeker.json'), 'utf8'));
+  const goLoadConfig = goSync(() => {
+    const rawConfig = JSON.parse(fs.readFileSync(join(configPath, airseekerConfigPath), 'utf8'));
     const secrets = parseSecrets(rawSecrets);
-    return configSchema.parseAsync(interpolateSecrets(rawConfig, secrets));
+    return configSchema.safeParse(interpolateSecrets(rawConfig, secrets));
   });
 
   if (!goLoadConfig.success) throw new Error(`Unable to load configuration.`, { cause: goLoadConfig.error });
---
+++ test/readme.test.ts
@@ -0,0 +1,38 @@
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+import { z } from 'zod';
+import { loadConfig } from '../src/config';
+
+/*
+ * Recursively extracts keys from an object.
+ */
+const extractKeys = (item: any): string[] =>
+  typeof item === typeof {}
+    ? [...Object.keys(item), ...Object.values(item).flatMap((element) => extractKeys(element))]
+    : [];
+
+describe('checks README', () => {
+  it('checks that the README contains all configuration keys in airseeker.example.json', () => {
+    const config = loadConfig(path.join(__dirname, '../config'), 'airseeker.example.json');
+    expect(config).toBeDefined();
+
+    const readmeData = readFileSync(path.join(__dirname, '../README.md')).toString();
+    expect(readmeData).toBeDefined();
+
+    // console.log(config);
+    const keys = extractKeys(config).filter((item) => !z.coerce.number().safeParse(item).success);
+
+    const missingKeys = keys.filter((key) => readmeData.includes(`\n#### ${key}`));
+
+    // eslint-disable-next-line jest/no-conditional-in-test
+    if (missingKeys.length > 0) {
+      // eslint-disable-next-line no-console
+      console.error(
+        `The following keys are present in airseeker.example.json, but not in the README:\n`,
+        missingKeys.join(', ')
+      );
+    }
+
+    expect(missingKeys).toHaveLength(0);
+  });
+});
