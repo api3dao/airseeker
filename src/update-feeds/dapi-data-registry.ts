e229b660ae3554f9bacd69c5668138414ee83aa9
Emanuel Tesař <e.tesarr@gmail.com>
2023-10-30 21:05:35 +0100

    Implement e2e test, fix bugs

--- src/update-feeds/dapi-data-registry.ts
+++ src/update-feeds/dapi-data-registry.ts
@@ -6,6 +6,43 @@ import { type DapiDataRegistry, DapiDataRegistry__factory } from '../../typechai
 export const getDapiDataRegistry = (address: string, provider: ethers.providers.StaticJsonRpcProvider) =>
   DapiDataRegistry__factory.connect(address, provider);
 
-export type ReadDapisResponse = Awaited<ReturnType<DapiDataRegistry['readDapis']>>;
+export const verifyMulticallResponse = (
+  response: Awaited<ReturnType<DapiDataRegistry['callStatic']['tryMulticall']>>
+) => {
+  const [successes, returndata] = response;
 
-export type DapisCountResponse = Awaited<ReturnType<DapiDataRegistry['dapisCount']>>;
+  if (!successes.every(Boolean)) throw new Error('One of the multicalls failed');
+  return returndata;
+};
+
+export const decodeDapisCountResponse = (dapiDataRegistry: DapiDataRegistry, dapisCountReturndata: string) => {
+  const dapisCount = dapiDataRegistry.interface.decodeFunctionResult('dapisCount', dapisCountReturndata)[0] as Awaited<
+    ReturnType<DapiDataRegistry['dapisCount']>
+  >;
+
+  return dapisCount.toNumber();
+};
+
+export type DapisCountResponse = ReturnType<typeof decodeDapisCountResponse>;
+
+export const decodeReadDapiWithIndexResponse = (
+  dapiDataRegistry: DapiDataRegistry,
+  readDapiWithIndexReturndata: string
+) => {
+  const { dapiName, updateParameters, dataFeedValue, dataFeed, signedApiUrls } =
+    dapiDataRegistry.interface.decodeFunctionResult('readDapiWithIndex', readDapiWithIndexReturndata) as Awaited<
+      ReturnType<DapiDataRegistry['readDapiWithIndex']>
+    >;
+
+  // Ethers responses are returned as a combination of array and object. When such object is logged, only the array part
+  // is logged. To make the logs more readable, we convert the object part to a plain object.
+  return {
+    dapiName,
+    updateParameters,
+    dataFeedValue,
+    dataFeed,
+    signedApiUrls,
+  };
+};
+
+export type ReadDapiWithIndexResponse = ReturnType<typeof decodeReadDapiWithIndexResponse>;
