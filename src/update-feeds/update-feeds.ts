0cb0ad904ee407f01d2417d1c8eace0d5694da23
aquarat <blue@aquarat.co.uk>
2023-10-31 10:15:41 +0000

    Remove unused code

--- src/update-feeds/update-feeds.ts
+++ src/update-feeds/update-feeds.ts
@@ -34,7 +34,8 @@ export const startUpdateFeedLoops = async () => {
     })
   );
 };
-
+// https://github.com/api3dao/dapi-management/pull/3/files#diff-b6941851ebc92dc9691bbf0cb701fe9c4595cb78488c3bb92ad6e4b917719f4fR374
+// TODO baseLogContext, actually test this against local node, add delay to reduce
 export const runUpdateFeed = async (providerName: string, chain: Chain, _chainId: string) => {
   // const { dataFeedBatchSize, dataFeedUpdateInterval, providers, contracts } = chain;
   const { providers, contracts } = chain;
@@ -80,54 +81,8 @@ export const runUpdateFeed = async (providerName: string, chain: Chain, _chainId
   );
 
   await Promise.allSettled(dapiTuples.responses.map(async (element) => processDApi(element)));
-
-  // logger.debug(`Fetching first batch of dAPIs batches`, baseLogContext);
-  // const firstBatchStartTime = Date.now();
-  // const goFirstBatch = await go(async () => {
-  //   // TODO: Use multicall to fetch this is a single RPC call.
-  //   return {
-  //     batch: await dapiDataRegistry.readDapis(0, dataFeedBatchSize),
-  //     // eslint-disable-next-line unicorn/no-await-expression-member
-  //     totalDapisCount: (await dapiDataRegistry.dapisCount()).toNumber(),
-  //   };
-  // });
-  // if (!goFirstBatch.success) {
-  //   logger.error(`Failed to get first active dAPIs batch`, goFirstBatch.error, baseLogContext);
-  //   return;
-  // }
-  // const { batch: firstBatch, totalDapisCount: totalCount } = goFirstBatch.data;
-  // const processFirstBatchPromise = processBatch(firstBatch);
-  //
-  // // Calculate the stagger time between the rest of the batches.
-  // const batchesCount = totalCount / dataFeedBatchSize;
-  // const staggerTime = batchesCount <= 1 ? 0 : (dataFeedUpdateInterval / batchesCount) * 1000;
-  //
-  // // Wait the remaining stagger time required after fetching the first batch.
-  // const firstBatchDuration = Date.now() - firstBatchStartTime;
-  // await sleep(Math.max(0, staggerTime - firstBatchDuration));
-  //
-  // // Fetch the rest of the batches in parallel in a staggered way.
-  // logger.debug('Fetching batches of active dAPIs', { batchesCount, staggerTime, ...baseLogContext });
-  // const otherBatches = await Promise.allSettled(
-  //   range(1, batchesCount).map(async (batchIndex) => {
-  //     await sleep((batchIndex - 1) * staggerTime);
-  //
-  //     logger.debug(`Fetching batch of active dAPIs`, { batchIndex, ...baseLogContext });
-  //     return dapiDataRegistry.readDapis(batchIndex * dataFeedBatchSize, dataFeedBatchSize);
-  //   })
-  // );
-  // for (const batch of otherBatches.filter((batch) => !isFulfilled(batch))) {
-  //   logger.error(`Failed to get active dAPIs batch`, (batch as PromiseRejectedResult).reason, baseLogContext);
-  // }
-  // const processOtherBatchesPromises = otherBatches
-  //   .filter((result) => isFulfilled(result))
-  //   .map(async (result) => processBatch((result as PromiseFulfilledResult<ReadDapisResponse>).value));
-  //
-  // // Wait for all the batches to be processed.
-  // //
-  // // TODO: Consider returning some information (success/error) and log some statistics (e.g. how many dAPIs were
-  // // updated, etc...).
-  // await Promise.all([processFirstBatchPromise, ...processOtherBatchesPromises]);
+  // Carried over from previous code
+  // TODO: Consider returning some information (success/error) and log some statistics (e.g. how many dAPIs were
 };
 
 export const processDApi = async (_dAPI: ReadDapiResponse) => {
