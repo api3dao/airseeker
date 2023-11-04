47896692727e82080be6e83c74e29f352c7727d2
Emanuel Tesař <e.tesarr@gmail.com>
2023-11-04 12:34:39 +0100

    Import Api3ServerV1 from dependencies

--- src/update-feeds/api3-server-v1.ts
+++ src/update-feeds/api3-server-v1.ts
@@ -1,7 +1,5 @@
+import { Api3ServerV1__factory } from '@api3/airnode-protocol-v1';
 import type { ethers } from 'ethers';
-
-// NOTE: The contract is not yet published, so we generate the Typechain artifacts locally and import it from there.
-import { Api3ServerV1__factory } from '../../typechain-types';
 
 export const getApi3ServerV1 = (address: string, provider: ethers.providers.StaticJsonRpcProvider) =>
   Api3ServerV1__factory.connect(address, provider);
