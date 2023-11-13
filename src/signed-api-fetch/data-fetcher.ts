afbb4401a7f986ea08730a15ef21d1b02616e3ae
Emanuel Tesař <e.tesarr@gmail.com>
2023-11-13 17:28:39 +0100

    Parse axios error response

--- src/signed-api-fetch/data-fetcher.ts
+++ src/signed-api-fetch/data-fetcher.ts
@@ -1,8 +1,8 @@
 import { clearInterval } from 'node:timers';
 
 import { go } from '@api3/promise-utils';
-import axios from 'axios';
-import { uniq } from 'lodash';
+import axios, { type AxiosResponse, type AxiosError } from 'axios';
+import { pick, uniq } from 'lodash';
 
 import { HTTP_SIGNED_DATA_API_ATTEMPT_TIMEOUT, HTTP_SIGNED_DATA_API_HEADROOM } from '../constants';
 import { logger } from '../logger';
@@ -12,6 +12,21 @@ import { signedApiResponseSchema, type SignedData } from '../types';
 
 // Express handler/endpoint path: https://github.com/api3dao/signed-api/blob/b6e0d0700dd9e7547b37eaa65e98b50120220105/packages/api/src/server.ts#L33
 // Actual handler fn: https://github.com/api3dao/signed-api/blob/b6e0d0700dd9e7547b37eaa65e98b50120220105/packages/api/src/handlers.ts#L81
+
+// Inspired by: https://axios-http.com/docs/handling_errors.
+//
+// The implementation differs by only picking fields that are important for debugging purposes to avoid cluttering the
+// logs.
+const parseAxiosError = (error: AxiosError) => {
+  const errorContext = pick(error, ['cause', 'code', 'name', 'message', 'stack', 'status']);
+
+  // The request was made and the server responded with a status code that falls out of the range of 2xx.
+  if (error.response) {
+    return { ...errorContext, response: pick(error.response, ['data', 'status', 'headers']) };
+  }
+
+  return errorContext;
+};
 
 /**
  * Shuts down intervals
@@ -24,8 +39,8 @@ export const stopDataFetcher = () => {
  * Calls a remote signed data URL and inserts the result into the datastore
  * @param url
  */
-const callSignedDataApi = async (url: string): Promise<SignedData[]> => {
-  const result = await go(
+const callSignedDataApi = async (url: string): Promise<SignedData[] | null> => {
+  const goAxiosCall = await go<Promise<AxiosResponse>, AxiosError>(
     async () =>
       axios({
         method: 'get',
@@ -42,12 +57,12 @@ const callSignedDataApi = async (url: string): Promise<SignedData[]> => {
     }
   );
 
-  // TODO: Pass the response error body (if available). See OEV repo for details.
-  if (!result.success) {
-    throw new Error([`HTTP call failed: `, url, result.error].join('\n'));
+  if (!goAxiosCall.success) {
+    logger.warn('Failed to fetch data from signed API', parseAxiosError(goAxiosCall.error));
+    return null;
   }
 
-  const { data } = signedApiResponseSchema.parse(result.data.data);
+  const { data } = signedApiResponseSchema.parse(goAxiosCall.data?.data);
 
   return Object.values(data);
 };
@@ -80,13 +95,14 @@ export const runDataFetcher = async () => {
 
     logger.debug('Fetching data from signed APIs', { urls });
     return Promise.all(
-      urls.map(async (url) => {
-        const goSignedApiCall = await go(
+      urls.map(async (url) =>
+        go(
           async () => {
-            const payload = await callSignedDataApi(url);
+            const signedDataApiResponse = await callSignedDataApi(url);
+            if (!signedDataApiResponse) return;
 
-            for (const element of payload) {
-              localDataStore.setStoreDataPoint(element);
+            for (const signedData of signedDataApiResponse) {
+              localDataStore.setStoreDataPoint(signedData);
             }
           },
           {
@@ -94,12 +110,8 @@ export const runDataFetcher = async () => {
             totalTimeoutMs: signedDataFetchIntervalMs + HTTP_SIGNED_DATA_API_HEADROOM,
             attemptTimeoutMs: signedDataFetchIntervalMs + HTTP_SIGNED_DATA_API_HEADROOM - 100,
           }
-        );
-
-        if (!goSignedApiCall.success) {
-          logger.warn(`Failed to fetch data from signed API`, { error: goSignedApiCall.error });
-        }
-      })
+        )
+      )
     );
   });
 };
