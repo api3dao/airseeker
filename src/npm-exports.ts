4315b2ea24fbe52dd4d35fc225af6851999bb841
Emanuel Tesař <e.tesarr@gmail.com>
2024-03-14 08:24:32 +0100

    Setup NPM publishing (#202)
    * Add prepublishOnly script
    
    * Configure publishing
    
    * Update release instructions

---
+++ src/npm-exports.ts
@@ -0,0 +1,4 @@
+// NOTE: This file defines the NPM exports. Everything that should be available to import from the NPM package should be
+// exported here.
+export * from './utils';
+export * from './config/schema';
