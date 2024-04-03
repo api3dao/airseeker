33fe7c82fc01c9d87cd1a8386334ef6cc68799e8
Emanuel Tesař <e.tesarr@gmail.com>
2024-04-03 18:21:15 +0200

    Fix dockerfile

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
+import { cwd } from 'node:process';
 
 import { goSync } from '@api3/promise-utils';
 import dotenv from 'dotenv';
@@ -7,7 +8,11 @@ import dotenv from 'dotenv';
 import { configSchema } from './schema';
 import { interpolateSecrets, parseSecrets } from './utils';
 
-export const getConfigPath = () => join(__dirname, '../../config');
+// When Airnode feed is built, the "/dist" file contains "src" folder and "package.json" and the config is expected to
+// be located next to the "/dist" folder. When run in development, the config is expected to be located next to the
+// "src" folder (one less import level). We resolve the config by CWD as a workaround. Since the Airnode feed is
+// dockerized, this is hidden from the user.
+const getConfigPath = () => join(cwd(), './config');
 
 export const loadRawConfig = () => JSON.parse(readFileSync(join(getConfigPath(), 'airseeker.json'), 'utf8'));
 
