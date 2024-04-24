93aa523158ee1613e3d0c428d2b95d64503e988b
Emanuel Tesař <e.tesarr@gmail.com>
2024-04-24 10:30:05 +0200

    Implement direct beacon set updates

--- src/update-feeds-loops/submit-transactions.ts
+++ src/update-feeds-loops/submit-transactions.ts
@@ -9,7 +9,7 @@ import { logger } from '../logger';
 import { getState, updateState } from '../state';
 import { deriveSponsorWallet } from '../utils';
 
-import type { UpdatableDataFeed } from './get-updatable-feeds';
+import type { UpdatableBeacon, UpdatableDataFeed } from './get-updatable-feeds';
 
 export const createUpdateFeedCalldatas = (api3ServerV1: Api3ServerV1, updatableDataFeed: UpdatableDataFeed) => {
   const { dataFeedInfo, updatableBeacons } = updatableDataFeed;
@@ -37,6 +37,64 @@ export const createUpdateFeedCalldatas = (api3ServerV1: Api3ServerV1, updatableD
     : beaconUpdateCalls;
 };
 
+export const submitUpdate = async (
+  api3ServerV1: Api3ServerV1,
+  updatableDataFeed: UpdatableDataFeed,
+  fallbackGasLimit: number | undefined,
+  connectedSponsorWallet: ethers.HDNodeWallet | ethers.Wallet,
+  gasPrice: bigint,
+  nonce: number
+) => {
+  const {
+    updatableBeacons,
+    dataFeedInfo: { beaconsWithData },
+  } = updatableDataFeed;
+  const sponsorWalletAddress = connectedSponsorWallet.address as Address;
+  const isSingleBeaconUpdate = beaconsWithData.length === 1;
+
+  if (isSingleBeaconUpdate) {
+    const beacon = updatableBeacons[0]!;
+
+    logger.debug('Estimating single beacon update gas limit.');
+    const gasLimit = await estimateSingleBeaconGasLimit(api3ServerV1, beacon, fallbackGasLimit);
+    if (!gasLimit) return null;
+
+    logger.info('Updating single beacon.', {
+      sponsorWalletAddress,
+      gasPrice: gasPrice.toString(),
+      gasLimit: gasLimit.toString(),
+      nonce,
+    });
+    const {
+      signedData: { airnode, templateId, timestamp, encodedValue, signature },
+    } = beacon;
+    return api3ServerV1
+      .connect(connectedSponsorWallet)
+      .updateBeaconWithSignedData(airnode, templateId, timestamp, encodedValue, signature, {
+        gasPrice,
+        gasLimit,
+        nonce,
+      });
+  }
+
+  logger.debug('Creating calldatas.');
+  const dataFeedUpdateCalldatas = createUpdateFeedCalldatas(api3ServerV1, updatableDataFeed);
+
+  logger.debug('Estimating beacon set update gas limit.');
+  const gasLimit = await estimateMulticallGasLimit(api3ServerV1, dataFeedUpdateCalldatas, fallbackGasLimit);
+  if (!gasLimit) return null;
+
+  logger.info('Updating data feed.', {
+    sponsorWalletAddress,
+    gasPrice: gasPrice.toString(),
+    gasLimit: gasLimit.toString(),
+    nonce,
+  });
+  return api3ServerV1
+    .connect(connectedSponsorWallet)
+    .tryMulticall.send(dataFeedUpdateCalldatas, { gasPrice, gasLimit, nonce });
+};
+
 export const submitTransaction = async (
   chainId: string,
   providerName: string,
@@ -60,13 +118,6 @@ export const submitTransaction = async (
     // handle errors internally.
     const goUpdate = await go(
       async () => {
-        logger.debug('Creating calldatas.');
-        const dataFeedUpdateCalldatas = createUpdateFeedCalldatas(api3ServerV1, updatableDataFeed);
-
-        logger.debug('Estimating gas limit.');
-        const gasLimit = await estimateMulticallGasLimit(api3ServerV1, dataFeedUpdateCalldatas, fallbackGasLimit);
-        if (!gasLimit) return null;
-
         logger.debug('Getting derived sponsor wallet.');
         const sponsorWallet = getDerivedSponsorWallet(
           sponsorWalletMnemonic,
@@ -77,7 +128,7 @@ export const submitTransaction = async (
         const sponsorWalletAddress = sponsorWallet.address as Address;
 
         logger.debug('Getting nonce.');
-        const goNonce = await go(async () => provider.getTransactionCount(sponsorWallet.address, blockNumber));
+        const goNonce = await go(async () => provider.getTransactionCount(sponsorWalletAddress, blockNumber));
         if (!goNonce.success) {
           logger.warn(`Failed to get nonce.`, goNonce.error);
           return null;
@@ -88,26 +139,24 @@ export const submitTransaction = async (
         const gasPrice = getRecommendedGasPrice(chainId, providerName, sponsorWalletAddress);
         if (!gasPrice) return null;
 
-        logger.info('Updating data feed.', {
-          sponsorWalletAddress,
-          gasPrice: gasPrice.toString(),
-          gasLimit: gasLimit.toString(),
-          nonce,
-        });
-        const goMulticall = await go(async () => {
-          return (
-            api3ServerV1
-              // When we add the sponsor wallet (signer) without connecting it to the provider, the provider of the
-              // contract will be set to "null". We need to connect the sponsor wallet to the provider of the contract.
-              .connect(sponsorWallet.connect(provider))
-              .tryMulticall.send(dataFeedUpdateCalldatas, { gasPrice, gasLimit, nonce })
+        const goSubmitUpdate = await go(async () => {
+          // When we add the sponsor wallet (signer) without connecting it to the provider, the provider of the
+          // contract will be set to "null". We need to connect the sponsor wallet to the provider of the contract.
+          const connectedSponsorWallet = sponsorWallet.connect(provider);
+          return submitUpdate(
+            api3ServerV1,
+            updatableDataFeed,
+            fallbackGasLimit,
+            connectedSponsorWallet,
+            gasPrice,
+            nonce
           );
         });
-        if (!goMulticall.success) {
+        if (!goSubmitUpdate.success) {
           // It seems that in practice, this code is widely used. We can do a best-effort attempt to determine the error
           // reason. Many times, the error is acceptable and results from the way Airseeker is designed. We can use
           // different log levels and messages and have better alerts.
-          const errorCode = (goMulticall.error as any).code;
+          const errorCode = (goSubmitUpdate.error as any).code;
           switch (errorCode) {
             case 'REPLACEMENT_UNDERPRICED': {
               logger.info(`Failed to submit replacement transaction because it was underpriced.`);
@@ -119,18 +168,19 @@ export const submitTransaction = async (
             }
             case 'INSUFFICIENT_FUNDS': {
               // This should never happen and monitoring should warn even before Airseeker comes to this point.
-              logger.error(`Failed to submit the transaction because of insufficient funds.`, goMulticall.error);
+              logger.error(`Failed to submit the transaction because of insufficient funds.`, goSubmitUpdate.error);
               return null;
             }
             default: {
-              logger.warn(`Failed to submit the multicall transaction.`, goMulticall.error);
+              logger.warn(`Failed to submit the update transaction.`, goSubmitUpdate.error);
               return null;
             }
           }
         }
 
-        logger.info('Successfully submitted the multicall transaction.', { txHash: goMulticall.data.hash });
-        return goMulticall.data;
+        if (!goSubmitUpdate.data) return null; // There was a handled error during submission.
+        logger.info('Successfully submitted the update transaction.', { txHash: goSubmitUpdate.data.hash });
+        return goSubmitUpdate.data;
       },
       { totalTimeoutMs: dataFeedUpdateIntervalMs }
     );
@@ -156,6 +206,46 @@ export const submitTransactions = async (
       submitTransaction(chainId, providerName, provider, api3ServerV1, dataFeed, blockNumber)
     )
   );
+
+export const estimateSingleBeaconGasLimit = async (
+  api3ServerV1: Api3ServerV1,
+  beacon: UpdatableBeacon,
+  fallbackGasLimit: number | undefined
+) => {
+  const { signedData } = beacon;
+
+  const goEstimateGas = await go(async () =>
+    api3ServerV1.updateBeaconWithSignedData.estimateGas(
+      signedData.airnode,
+      signedData.templateId,
+      signedData.timestamp,
+      signedData.encodedValue,
+      signedData.signature
+    )
+  );
+  if (goEstimateGas.success) {
+    return BigInt(goEstimateGas.data);
+  }
+  const errorMessage = goEstimateGas.error.message;
+  // It is possible that the gas estimation failed because of a contract revert due to timestamp check, because the feed
+  // was updated by other provider in the meantime. Try to detect this expected case and log INFO instead.
+  if (errorMessage.includes('Does not update timestamp')) {
+    logger.info(`Gas estimation failed because of a contract revert.`, { errorMessage });
+  } else {
+    logger.warn(`Unable to estimate gas for single beacon using provider.`, { errorMessage });
+  }
+
+  // TODO: Avoid repetition
+  if (!fallbackGasLimit) {
+    // Logging it as an INFO because in practice this would result in double logging of the same issue. If there is no
+    // fallback gas limit specified it's expected that the update transcation will be skipped in case of gas limit
+    // estimation failure.
+    logger.info('No fallback gas limit provided. No gas limit to use.');
+    return null;
+  }
+
+  return BigInt(fallbackGasLimit);
+};
 
 export const estimateMulticallGasLimit = async (
   api3ServerV1: Api3ServerV1,
