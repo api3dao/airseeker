d0b5a0c5bd51537b3118b9daf66d32cc672efdbb
Emanuel Tesař <e.tesarr@gmail.com>
2024-04-24 10:36:40 +0200

    Avoid repetition

--- src/update-feeds-loops/submit-transactions.ts
+++ src/update-feeds-loops/submit-transactions.ts
@@ -207,6 +207,27 @@ export const submitTransactions = async (
     )
   );
 
+export const handleRpcGasLimitFailure = (error: Error, fallbackGasLimit: number | undefined) => {
+  const errorMessage = error.message;
+  // It is possible that the gas estimation failed because of a contract revert due to timestamp check, because the feed
+  // was updated by other provider in the meantime. Try to detect this expected case and log INFO instead.
+  if (errorMessage.includes('Does not update timestamp')) {
+    logger.info(`Gas estimation failed because of a contract revert.`, { errorMessage });
+  } else {
+    logger.warn(`Unable to estimate gas for single beacon using provider.`, { errorMessage });
+  }
+
+  if (!fallbackGasLimit) {
+    // Logging it as an INFO because in practice this would result in double logging of the same issue. If there is no
+    // fallback gas limit specified it's expected that the update transcation will be skipped in case of gas limit
+    // estimation failure.
+    logger.info('No fallback gas limit provided. No gas limit to use.');
+    return null;
+  }
+
+  return BigInt(fallbackGasLimit);
+};
+
 export const estimateSingleBeaconGasLimit = async (
   api3ServerV1: Api3ServerV1,
   beacon: UpdatableBeacon,
@@ -223,28 +244,8 @@ export const estimateSingleBeaconGasLimit = async (
       signedData.signature
     )
   );
-  if (goEstimateGas.success) {
-    return BigInt(goEstimateGas.data);
-  }
-  const errorMessage = goEstimateGas.error.message;
-  // It is possible that the gas estimation failed because of a contract revert due to timestamp check, because the feed
-  // was updated by other provider in the meantime. Try to detect this expected case and log INFO instead.
-  if (errorMessage.includes('Does not update timestamp')) {
-    logger.info(`Gas estimation failed because of a contract revert.`, { errorMessage });
-  } else {
-    logger.warn(`Unable to estimate gas for single beacon using provider.`, { errorMessage });
-  }
-
-  // TODO: Avoid repetition
-  if (!fallbackGasLimit) {
-    // Logging it as an INFO because in practice this would result in double logging of the same issue. If there is no
-    // fallback gas limit specified it's expected that the update transcation will be skipped in case of gas limit
-    // estimation failure.
-    logger.info('No fallback gas limit provided. No gas limit to use.');
-    return null;
-  }
-
-  return BigInt(fallbackGasLimit);
+  if (goEstimateGas.success) return BigInt(goEstimateGas.data);
+  return handleRpcGasLimitFailure(goEstimateGas.error, fallbackGasLimit);
 };
 
 export const estimateMulticallGasLimit = async (
@@ -257,24 +258,7 @@ export const estimateMulticallGasLimit = async (
     // Adding a extra 10% because multicall consumes less gas than tryMulticall
     return (goEstimateGas.data * BigInt(Math.round(1.1 * 100))) / 100n;
   }
-  const errorMessage = goEstimateGas.error.message;
-  // It is possible that the gas estimation failed because of a contract revert due to timestamp check, because the feed
-  // was updated by other provider in the meantime. Try to detect this expected case and log INFO instead.
-  if (errorMessage.includes('Does not update timestamp')) {
-    logger.info(`Gas estimation failed because of a contract revert.`, { errorMessage });
-  } else {
-    logger.warn(`Unable to estimate gas for multicall using provider.`, { errorMessage });
-  }
-
-  if (!fallbackGasLimit) {
-    // Logging it as an INFO because in practice this would result in double logging of the same issue. If there is no
-    // fallback gas limit specified it's expected that the update transcation will be skipped in case of gas limit
-    // estimation failure.
-    logger.info('No fallback gas limit provided. No gas limit to use.');
-    return null;
-  }
-
-  return BigInt(fallbackGasLimit);
+  return handleRpcGasLimitFailure(goEstimateGas.error, fallbackGasLimit);
 };
 
 export const getDerivedSponsorWallet = (
