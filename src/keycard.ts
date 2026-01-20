f035dc0c35c3377d1f10fcc37d7a415fbd7543d9
bdrhn9 <bedirhan@api3.org>
2026-01-20 12:46:15 +0000

    Throw error if keycard is already initialized

--- src/keycard.ts
+++ src/keycard.ts
@@ -5,15 +5,18 @@ import { loadConfig } from './config';
 let keycardWallet: KeycardWallet | undefined;
 
 export const initializeKeycardWallet = async () => {
+  if (keycardWallet) {
+    throw new Error('Keycard wallet is already initialized.');
+  }
+
   const { walletDerivationScheme } = loadConfig();
   if (walletDerivationScheme.type !== 'keycard') {
     throw new Error('Wallet derivation scheme is not keycard. This function should not be called.');
   }
 
-  if (keycardWallet) return;
-
-  // Do not import at the top level to avoid loading keycard-manager when not needed
-  keycardWallet = await (await import('keycard-manager')).getKeycardWallet(undefined, walletDerivationScheme.pin); // eslint-disable-line
+  // Dynamic import to avoid loading keycard-manager when not needed
+  const { getKeycardWallet: createKeycardWallet } = await import('keycard-manager');
+  keycardWallet = await createKeycardWallet(undefined, walletDerivationScheme.pin);
 };
 
 export const getKeycardWallet = () => {
